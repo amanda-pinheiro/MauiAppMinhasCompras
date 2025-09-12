0e9ce89123a4ce6a8ba88258a336b4a351310cf8
AmandaPinheiro <amandadejesuspinheiro@outlook.com>
2025-09-11 22:19:55 -0300

    Alterações iniciais da atividade da semana

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -11,7 +11,7 @@ namespace MauiAppMinhasCompras.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
-        public string Descricao { 
+        public string Descricao
             get => _descricao;
             set 
             {
